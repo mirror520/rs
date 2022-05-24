6186d0fe78b0dbdb4d437ffefa62b085c6276242
Lin, Ying-Chin <mirror770109@gmail.com>
2022-05-24 21:34:17 +0800

    加入 sentence.Service 支援 HTTP 傳輸協定

--- sentence/service.go
+++ sentence/service.go
@@ -1,20 +1,27 @@
 package sentence
 
-import "context"
+import (
+	"context"
+)
 
 type Service interface {
 	GetSentence(ctx context.Context) (string, error)
 }
 
 type service struct {
+	thirdService Service
 }
 
-func NewService() Service {
-	return new(service) // 檢查 service 是否已實作 Service 介面所有方法
+func NewService(third Service) Service {
+	svc := new(service)
+	svc.thirdService = third
+
+	// 可檢查 service 是否已實作 Service 介面所有方法
+	return svc
 }
 
 func (svc *service) GetSentence(ctx context.Context) (string, error) {
-	return "", nil
+	return svc.thirdService.GetSentence(ctx)
 }
 
 type ServiceMiddleware func(Service) Service
